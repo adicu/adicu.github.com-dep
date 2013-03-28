720b6842f8614551dc9bd188f72046cc22b8fffc
Howard Mao <zhehao.mao@gmail.com>
2013-03-28 11:00:03 -0400

    get rid of newpost script

--- newpost.sh
+++
@@ -1,8 +0,0 @@
-if [ -n "$1" ]; then
-	FNAME="_posts/`date +%Y-%m-%d-$1.markdown`"
-	echo '---' > $FNAME
-	echo 'title:' >> $FNAME
-	echo 'layout: post' >> $FNAME
-	echo '---' >> $FNAME
-    echo "New post at $FNAME"
-fi
