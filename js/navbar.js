bbbb07811ede6c945c0958df84cae9ae7c889b90
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-11-03 12:10:20 -0500

    add social icons and fix navbar on mobile

--- js/navbar.js
+++ js/navbar.js
@@ -5,13 +5,13 @@ $(function(){
   var $spacer = $('.navbar-spacer');
   
   $win.scroll(function() {     
-    if(!$navbar.hasClass('navbar-fixed-top') && $win.scrollTop() > $navbar.position().top){
+    if(!$navbar.hasClass('navbar-fixed-top') && $win.scrollTop() > $navbar.offset().top){
       console.log("add");
       $banner.addClass("spacer-active")
       $spacer.addClass("spacer-active");
       $navbar.addClass("navbar-fixed-top");
 
-    } else if ($navbar.hasClass('navbar-fixed-top')  && $win.scrollTop() < $spacer.position().top){
+    } else if ($navbar.hasClass('navbar-fixed-top')  && $win.scrollTop() < $spacer.offset().top){
       console.log("remove");
       $navbar.removeClass("navbar-fixed-top");
       $banner.removeClass("spacer-active")
