ce7932a32f2b32b1221c054b076a807802e28f48
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-11-05 20:29:07 -0500

    hide listserv in the navbar at the top of the page.

--- js/navbar.js
+++ js/navbar.js
@@ -3,19 +3,24 @@ $(function(){
   var $banner = $('.adi-banner');
   var $navbar = $('.navbar');
   var $spacer = $('.navbar-spacer');
+  var $bannerListserv = $('.listserv');
   
-  $win.scroll(function() {     
+  $win.scroll( function() {   
     if(!$navbar.hasClass('navbar-fixed-top') && $win.scrollTop() > $navbar.offset().top){
       console.log("add");
-      $banner.addClass("spacer-active")
+      $banner.addClass("spacer-active");
       $spacer.addClass("spacer-active");
       $navbar.addClass("navbar-fixed-top");
+      $bannerListserv.removeClass("hidden");
 
     } else if ($navbar.hasClass('navbar-fixed-top')  && $win.scrollTop() < $spacer.offset().top){
       console.log("remove");
       $navbar.removeClass("navbar-fixed-top");
-      $banner.removeClass("spacer-active")
+      $banner.removeClass("spacer-active");
       $spacer.removeClass("spacer-active");
+      if (! $bannerListserv.hasClass("open")) {
+        $bannerListserv.addClass("hidden");
+      }
     }
   });
 });
