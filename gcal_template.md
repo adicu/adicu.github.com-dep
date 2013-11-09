a5e4af209d5311252a68c1b35b79395ae79ca6b1
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-11-08 20:16:03 -0500

    improve navbar/social and implement events as a carousel

---
+++ gcal_template.md
@@ -0,0 +1,11 @@
+short desc
+
+---
+
+long desc
+
+---
+
+slides: http://slidesurl.com/slides.html
+
+other link: http://otherlink.com/link.html  
---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	/* Configure bxSlider */
+  	$('.bxslider').bxSlider( {
+  		minSlides: 1,
+  		maxSlides: 10,
+  		slideWidth: 300,
+  		slideMargin: 10,
+  		pager: false,
+  		infiniteLoop: false,
+  		hideControlOnEnd: true
+  	});
+});
