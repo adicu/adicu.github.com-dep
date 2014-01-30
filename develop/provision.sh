123346c69b6e5cf5db9ca556d09fdf56d01e21fe
samw7823 <srwiener@gmail.com>
2014-01-29 23:31:09 -0500

    Set up Vagrant and provisioning script

---
+++ develop/provision.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Refresh package list
+apt-get -y update
+
+# Install Ruby and app dependencies
+apt-get -y install curl
+curl -sSL https://get.rvm.io | bash
+source /etc/profile.d/rvm.sh
+rvm install 2.0.0
+rvm use 2.0.0
+rvm alias create adicu 2.0.0
+gem install bundler
+cd /vagrant
+bundle install
+
+# Install Node.js and app dependencies
+apt-get -y install build-essential
+command -v npm > /dev/null
+if [ $? -ne 0 ]; then
+	mkdir /tmp/nodejs-install
+	cd /tmp/nodejs-install
+	curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+	./configure
+	make install
+	curl https://npmjs.org/install.sh | clean=no sh
+fi
+cd /vagrant
+sudo npm install
+
+#Install supervisor to make sure grunt and jekyll are running
+apt-get -y install supervisor
+cp /vagrant/develop/grunt.conf /etc/supervisor/conf.d
+cp /vagrant/develop/jekyll.conf /etc/supervisor/conf.d
+service supervisor stop
+sleep 3
+service supervisor start
