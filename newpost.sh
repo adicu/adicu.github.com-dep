c48162e336575e55787fe4ef53b15e60b7e5efd3
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-04-02 01:13:34 -0400

    update newpost.sh to check input validity and support new posting system

--- newpost.sh
+++ newpost.sh
@@ -1,15 +1,61 @@
 #!/bin/bash
 
-if [ -z "$2" ]; then
-	echo "Usage: $0 title type"
+# User input
+TYPE=$1
+NAME=$2
+
+# Types and Templates supported
+TYPES=( "blogpost" "resource" "link" )
+TEMPLATES=("_templates/blogpost.md" "_templates/resource.md" "_templates/link.md" )
+
+usage () {
+	# Print usage and quit
+	SAVE_IFS=$IFS
+	IFS="|"
+	TYPESJOIN="${TYPES[*]}"
+	IFS=$SAVE_IFS
+	echo "================================================"
+	echo "Usage: $0 $TYPESJOIN <name>"
+	echo "	Where '<name>' is all lowercase letters separated by dashes (-)"
+	echo "	ie:	$0 blogpost my-really-cool-post"
+	echo "================================================"
 	exit 1
+}
+
+indexOfElement () {
+	# Index of TYPE in TYPES or -1 for non-membership
+
+	for (( i = 0; i < ${#TYPES[@]}; i++ )); do
+		# echo "${TYPES[$i]}"
+   		if [ "${TYPES[$i]}" == "$TYPE" ]; then
+			echo $i
+			return
+		fi
+	done
+	echo "-1"
+}
+
+INDEX=$(indexOfElement)
+
+# If invalid input, print usage and quit
+if [[ "$#" -ne 2 || "$INDEX" -eq -1 ]];then
+	usage
+fi
+if ! [[ "$NAME" ~= "([a-z]|-)*" ]]; then
+	echo "Invalid name: $NAME"
+	usage
 fi
 
-TITLE=$1
-TYPE=$2
-TEMPLATE="_templates/$TYPE"
-POST="_posts/$(date +%Y-%m-%d)-$TITLE.${TYPE##*.}"
+TEMPLATE="${TEMPLATES["$INDEX"]}"
+POST="_posts/$(date +%Y-%m-%d)-$TITLE.${TEMPLATE##*.}"
 
-cp "$TEMPLATE" "$POST"
+# Special case for resource posts, they go to /learn/
+if [ "$TYPE" == "resource" ]; then
+	POST="learn/_posts/$(date +%Y-%m-%d)-$NAME.${TEMPLATE##*.}"
+	cp "$TEMPLATE" "$POST"
+	sed -i '' -e "s/^category.*$/&\\`echo -e '\n\r'`permalink: \/learn\/$NAME\//g" "$POST"
+else
+	cp "$TEMPLATE" "$POST"
+fi
 
-echo "Created new post in $POST"
+echo "Created new $TYPE in $POST"
